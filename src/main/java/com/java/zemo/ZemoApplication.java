1a59559b12ef013908e93536b82d2242f021b837
chenlu4ever <1045038404@qq.com>
2021-07-22 17:11:55 +0800

    初始化项目

---
+++ src/main/java/com/java/zemo/ZemoApplication.java
@@ -0,0 +1,13 @@
+package com.java.zemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZemoApplication.class, args);
+    }
+
+}
